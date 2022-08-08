fc90e47e9e5d2ef9f4eb7815203ae80b4068bdec
kokkenro0520@gmail.com <kokkenro0520@gmail.com>
2022-08-08 15:15:12 +0900

    feat(mcc): パーサーとコードジェネレーターを導入

--- mcc.c
+++ mcc.c
@@ -3,6 +3,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//
+// Tokenizer
+//
 
 typedef enum {
   TK_RESERVED,  // Keywords or punctuators,
@@ -66,7 +71,7 @@ Token *tokenize(char *p) {
       continue;
     }
     // Punctuator
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-", *p)) {
       cur = new_token(TK_RESERVED, cur, p);
       p++;
       continue;
@@ -84,13 +89,49 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
+//
+// Parser
+//
+
+typedef enum {
+  ND_ADD,  // +
+  ND_SUB,  // -
+  ND_MUL,  // *
+  ND_DIV,  // /
+  ND_NUM,  // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+  NodeKind kind;  // Node kind
+  Node *lhs;      // Left-hand side
+  Node *rhs;      // Right-hand side
+  int val;        // Used if kind == ND_NUM
+};
+
+Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
 // Consumes the current token if it matches `op`.
 bool consume(char op) {
-  if (token->kind != TK_RESERVED) {
-    error_at(token->str, "op is not TK_RESERVED");
-    return false;
-  }
-  if (token->str[0] != op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
     return false;
   }
   token = token->next;
@@ -120,6 +161,54 @@ int expect_number() {
 
 bool at_eof() { return token->kind == TK_EOF; }
 
+Node *expr();
+Node *primary();
+
+// expr = primary ("+" primary | "-" primary)*
+Node *expr() {
+  Node *node = primary();
+  for (;;) {
+    if (consume('+')) {
+      node = new_binary(ND_ADD, node, primary());
+      continue;
+    } else if (consume('-')) {
+      node = new_binary(ND_SUB, node, primary());
+      continue;
+    } else {
+      return node;
+    }
+  }
+}
+
+// primary = num
+Node *primary() { return new_num(expect_number()); }
+
+// Code Generator
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    default:
+      error("invalid node kind. kind: %s", node->kind);
+      break;
+  }
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません");
@@ -137,20 +226,23 @@ int main(int argc, char **argv) {
   //   token = token->next;
   // }
 
+  Node *node = expr();
+
+  // for dubug
+  // while (node) {
+  //   printf("Kind:%d\n", node->kind);
+  //   node = node->lhs;
+  // }
+
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", expect_number());
+  // Traverse the AST to emit assembly.
+  gen(node);
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
+  // A result must be at the top of the stack, so pop it
+  // to RAX to make it a program exit code.
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
