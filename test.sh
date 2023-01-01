66f5eb5da104cdb894f0b85a548350239533e40f
kokkenro0520@gmail.com <kokkenro0520@gmail.com>
2023-01-01 10:28:32 +0900

    chore(mcc): modify style

--- test.sh
+++ test.sh
@@ -28,53 +28,53 @@ assert() {
   fi
 }
 
-assert 0 "main() { return 0; }"
-assert 42 "main() { return 42; }"
-assert 21 "main() { return 5+20-4; }"
-assert 21 "main() { return  5 + 20 - 4 ; }"
-assert 12 "main() { return 4*3; }"
-assert 3 "main() { return 12/4; }"
-assert 16 "main() { return (5+3)*2; }"
-assert 10 "main() { return -10+20; }"
+assert 0 'main() { return 0; }'
+assert 42 'main() { return 42; }'
+assert 21 'main() { return 5+20-4; }'
+assert 21 'main() { return  5 + 20 - 4 ; }'
+assert 12 'main() { return 4*3; }'
+assert 3 'main() { return 12/4; }'
+assert 16 'main() { return (5+3)*2; }'
+assert 10 'main() { return -10+20; }'
 
-assert 1 "main() { return 1 == 1; }"
-assert 0 "main() { return 1 != 1; }"
-assert 1 "main() { return 1 < 2; }"
-assert 0 "main() { return 1 > 2; }"
-assert 1 "main() { return 1 <= 1; }"
-assert 1 "main() { return 1 >= 1; }"
-assert 0 "main() { return 1 <= -1; }"
+assert 1 'main() { return 1 == 1; }'
+assert 0 'main() { return 1 != 1; }'
+assert 1 'main() { return 1 < 2; }'
+assert 0 'main() { return 1 > 2; }'
+assert 1 'main() { return 1 <= 1; }'
+assert 1 'main() { return 1 >= 1; }'
+assert 0 'main() { return 1 <= -1; }'
 
-assert 3 "main() { a=3; return a; }"
-assert 12 "main() { a=3; z=4; return a*z; }"
+assert 3 'main() { a=3; return a; }'
+assert 12 'main() { a=3; z=4; return a*z; }'
 
-assert 6 "main() { foo = 1; bar = 2 + 3; return foo + bar; }"
-assert 12 "main() { foo1 = 3; BAR = 4; return foo1 * BAR; }"
+assert 6 'main() { foo = 1; bar = 2 + 3; return foo + bar; }'
+assert 12 'main() { foo1 = 3; BAR = 4; return foo1 * BAR; }'
 
-assert 3 "main() { if (0) return 2; return 3; }"
-assert 3 "main() { if (1-1) return 2; return 3; }"
-assert 2 "main() { if (1) return 2; return 3; }"
-assert 2 "main() { if (2-1) return 2; return 3; }"
-assert 3 "main() { if (0) return 2; else return 3; }"
-assert 2 "main() { if (1) return 2; else return 3; }"
+assert 3 'main() { if (0) return 2; return 3; }'
+assert 3 'main() { if (1-1) return 2; return 3; }'
+assert 2 'main() { if (1) return 2; return 3; }'
+assert 2 'main() { if (2-1) return 2; return 3; }'
+assert 3 'main() { if (0) return 2; else return 3; }'
+assert 2 'main() { if (1) return 2; else return 3; }'
 
-assert 3 "main() { {1; {2;} return 3;} }"
+assert 3 'main() { {1; {2;} return 3;} }'
 
-assert 10 "main() { i=0; while(i<10) i=i+1; return i; }"
-assert 55 "main() { i=0; j=0; while(i<=10) {j=i+j; i=i+1;} return j; }"
+assert 10 'main() { i=0; while(i<10) i=i+1; return i; }'
+assert 55 'main() { i=0; j=0; while(i<=10) {j=i+j; i=i+1;} return j; }'
 
-assert 55 "main() { i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j; }"
-assert 3 "main() { for (;;) return 3; return 5; }"
+assert 55 'main() { i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j; }'
+assert 3 'main() { for (;;) return 3; return 5; }'
 
-assert 3 "main() { return ret3(); }"
-assert 5 "main() { return ret5(); }"
-assert 8 "main() { return add(3, 5); }"
-assert 2 "main() { return sub(5, 3); }"
-assert 21 "main() { return add6(1,2,3,4,5,6); }"
+assert 3 'main() { return ret3(); }'
+assert 5 'main() { return ret5(); }'
+assert 8 'main() { return add(3, 5); }'
+assert 2 'main() { return sub(5, 3); }'
+assert 21 'main() { return add6(1,2,3,4,5,6); }'
 
-assert 32 "main() { return ret32(); } ret32() { return 32; }"
-assert 7 "main() { return add2(3,4); } add2(x,y) { return x+y; }"
-assert 1 "main() { return sub2(4,3); } sub2(x,y) { return x-y; }"
-assert 55 "main() { return fib(9); } fib(x) { if (x<=1) return 1; return fib(x-1) + fib(x-2); }"
+assert 32 'main() { return ret32(); } ret32() { return 32; }'
+assert 7 'main() { return add2(3,4); } add2(x,y) { return x+y; }'
+assert 1 'main() { return sub2(4,3); } sub2(x,y) { return x-y; }'
+assert 55 'main() { return fib(9); } fib(x) { if (x<=1) return 1; return fib(x-1) + fib(x-2); }'
 
 echo OK
